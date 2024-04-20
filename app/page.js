23a6fcc7a420d105c4d7a6da4df81d233a792ff4
Krystof Jezdik <jezdik.kr.2021@skola.ssps.cz>
2024-04-20 15:08:43 +0200

    feat: Basic blokpost formular
    Nejvic zakladni UI pro vytvoreni blogpostu. Nefunguje s markdown editorem.

--- app/page.js
+++ app/page.js
@@ -11,10 +11,18 @@ export default function Home() {
     });
   }
 
+  const createBlogpost = async() => {
+    const response = await fetch('http://localhost:8080/auth/logout', {
+      method: 'GET',
+      credentials: 'include'
+    });
+  }
+
   return (
     <main>
       <p>Main page!1!!</p>
       <button onClick={handleLogout}>LOGOUT</button>
+      <button onClick={createBlogpost}>Create blogpost</button>
     </main>
   );
 }
