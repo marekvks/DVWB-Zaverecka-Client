fb44d52cd0c230d1831a287b9aa4843e5de24ed7
H0nzab <honzab@hotmail.com>
2024-04-20 21:45:42 +0200

    POST endpoint
    endpoint for changing userData

--- app/userData/page.js
+++ app/userData/page.js
@@ -4,32 +4,67 @@ import { useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
 
 export default function UserData() {
+    const [userData, setUserData] = useState({});
+    const accessToken = Cookies.get("accessToken");
 
-    const [userData, setUserData] = useState({})
-    const AccesToken = Cookies.get("accessToken");
     useEffect(() => {
         (async () => {
             const response = await fetch('http://localhost:8080/user/getUser', {
                 headers: {
-                    "Authorization": `Bearer ${AccesToken}`
+                    "Authorization": `Bearer ${accessToken}`
                 }
+            });
+            const data = await response.json();
+            setUserData(data);
+        })();
+    }, []);
 
-            })
-            const data = await response.json()
-            setUserData(data)
-        })()
-    }, [])
-  return (
-    <main>
-        Jmenuji se {userData.firstName}
-        <br/>
-        Ale říkej mi {userData.username}
-        <br/>
-        Kontaktuj mě tady: {userData.email}
-        <br/>
-        Kdybys chtěl moje heslo: {userData.password}
-        <br/>
-        Tady v aplikaci jsem {userData.role}!!!
-    </main>
-  );
+    function MakeEditable(elementId) {
+        const element = document.getElementById(elementId);
+        element.contentEditable = "true";
+        element.focus();
+    }
+
+    async function saveAll() {
+        const updatedData = {
+            firstName: document.getElementById('name').textContent,
+            username: document.getElementById('username').textContent
+        };
+    
+        const response = await fetch('http://localhost:8080/user/updateUser', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${Cookies.get("accessToken")}`
+            },
+            body: JSON.stringify(updatedData)
+        });
+    
+        if (response.ok) {
+            console.log("User updated successfully.");
+        } else {
+            console.error("Failed to update user.");
+        }
+    }
+    return (
+        <main>
+            <div>
+                Jmenuji se: <p id='name'>{userData.firstName}</p>
+                <button id="nameBtn" onClick={() => MakeEditable('name')}>Edit Text</button>
+            </div>
+            <br/>
+            <div>
+                Ale říkej mi: <p id='username'>{userData.username}</p>
+                <button id="usernameBtn" onClick={() => MakeEditable('username')}>Edit Text</button>
+            </div>
+            <br/>
+            <p id='mail'>Kontaktuj mě tady: {userData.email}</p>
+            <br/>
+            <p id='password'>Kdybys chtěl moje heslo: {userData.password}</p>
+            <br/>
+            <p id='role'>Tady v aplikaci jsem {userData.role}!!!</p>
+            <br/>
+            <button id='saveBtn' onClick={() => saveAll()}>save</button>
+        </main>
+    );
 }
