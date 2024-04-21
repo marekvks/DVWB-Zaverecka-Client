71472ede4a8df90623f3c77975b4df36db0dd582
Marek Vykysalý <marek.vykysaly12@gmail.com>
2024-04-21 13:36:46 +0200

    chore: update 404 - raccoon

--- app/not-found.js
+++ app/not-found.js
@@ -4,9 +4,9 @@ import '../app/css/404.css';
 export default function NotFound() {
     return (
         <main>
-            <Image src="/404.gif" alt="404" width="500" height="500" />
+            <Image src="/raccoon-dance.gif" alt="404" width="500" height="500" />
             <h3>404 Not Found.</h3>
-            <p>Look at what you did, bro is angwy because of you :(</p>
+            <p>Nothing but a raccoon and an idiot vibing together. 😱</p>
         </main>
     )
 }
