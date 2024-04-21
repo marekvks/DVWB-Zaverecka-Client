4ba510d8596477771a7cbe558aba94c4b39ec371
Krystof Jezdik <jezdik.kr.2021@skola.ssps.cz>
2024-04-21 19:45:46 +0200

    feat: Feed setup

---
+++ app/blogPostFeed/page.js
@@ -0,0 +1,19 @@
+'use client'
+import React from 'react'
+import '../css/blogpost.css';
+import Cookies from 'js-cookie';
+import { getAccessToken } from '../lib/auth';
+import { marked } from 'marked';
+import DOMPurify from 'dompurify';
+
+const Feed = () => {
+
+
+  return (
+   <h2>asd</h2>
+
+    
+  )
+}
+
+export default Feed;
