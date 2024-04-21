31686f78724186a5894773c7e23521a70df2a133
Marek Vykysalý <marek.vykysaly12@gmail.com>
2024-04-21 02:32:47 +0200

    chore: complete navbar styling

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -2,47 +2,83 @@
 
 import Cookies from 'js-cookie';
 import { getAccessToken } from '../lib/auth';
-import { useEffect } from 'react';
+import { useLayoutEffect, useState } from 'react';
 
 import '../css/navbar.css';
 
 const userData = async () => {
     const accessToken = await getAccessToken(Cookies);
-    console.log(Cookies.get('accessToken'));
-    console.log(accessToken);
 
-    const response = await fetch('http://localhost:8080/auth/loggedIn', {
+    const response = await fetch('http://localhost:8080/user/getUser', {
         headers: {
             'Authorization': `Bearer ${accessToken}`
         }
     });
 
-    console.log(response.status);
+    if (response.status != 200)
+        return null;
+
+    const data = await response.json();
+
+    if (data)
+        return data;
+
+    return null;
 }
 
 export default function Navbar() {
+    const [user, setUser] = useState({});
 
-    useEffect(() => {
+    useLayoutEffect(() => {
         (async () => {
             const data = await userData();
+            if (!data)
+                return;
+
+            const currentUser = {
+                username: data.username
+            }
+
+            setUser(currentUser)
         })();
     }, []);
 
 
+    if (!user.username) {
+        return (
+            <header>
+                <nav>
+                    <div className="scale-container">
+                        <span className="logo">BlogPost</span>
+                        <div className="links">
+                            <div className="default-links">
+                                <a href="#">about</a>
+                                <a href="#">support</a>
+                            </div>
+                            <a className="login-link" href="/login">Login</a>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+        );
+    }
+
     return (
         <header>
-            <nav>
-                <div className="scale-container">
-                    <span className="logo">BlogPost</span>
-                    <div className="links">
-                        <div className="default-links">
-                            <a href="#">about</a>
-                            <a href="#">support</a>
+                <nav>
+                    <div className="scale-container">
+                        <span className="logo">BlogPost</span>
+                        <div className="links">
+                            <div className="default-links">
+                                <a href="#">about</a>
+                                <a href="#">support</a>
+                            </div>
+                            <div className="user-data">
+                                <a className="username" href="/userData">{user.username}</a>
+                            </div>
                         </div>
-                        <a className="login-link" href="/login">Login</a>
                     </div>
-                </div>
-            </nav>
-        </header>
+                </nav>
+            </header>
     )
 }
