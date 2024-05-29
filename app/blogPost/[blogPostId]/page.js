93c231df02dc91a889d21beedf0b923f72e27692
Marek Vykysalý <marek.vykysaly12@gmail.com>
2024-05-29 16:57:07 +0200

    fix: loading data when viewing single blogpost

--- app/blogPost/[blogPostId]/page.js
+++ app/blogPost/[blogPostId]/page.js
@@ -1,31 +1,39 @@
 'use client'
 
 import React, { useEffect, useState } from 'react';
+import DOMPurify from 'dompurify';
 import '@/css/blogPost.css';
 import { marked } from 'marked';
 import Link from 'next/link';
 
 
-export default async function BlogPostDetails({params}){
+export default function BlogPostDetails({params}){
 
+    const [blogPost, setBlogPost] = useState({});
+    const [htmlOutput, setHtmlOutput] = useState('');
 
-    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/blogPost/` + params.blogPostId,
-    {next: {revalidate: 5}});
-    const blogPost = await res.json();
+    const getBlogPost = async () => {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/blogPost/` + params.blogPostId);
 
+        const blogPost = await response.json();
+        setBlogPost(blogPost);
+        const parsedInput = marked.parse(blogPost.content);
+        setHtmlOutput(DOMPurify.sanitize(parsedInput));
+      }
     
-    const input = blogPost[0].content;
-    const htmlOutput = marked.parse(input);
-
-
+      useEffect(() => {
+        (async () => {
+          await getBlogPost();
+        })();
+      }, []);
 
     return(
         <div className='blogPost'>
-            <h1>{blogPost[0].title}</h1>
-            <p className='description'>{blogPost[0].description}</p>
-            <div dangerouslySetInnerHTML={{ __html: htmlOutput }} />
+            <h1>{blogPost.title}</h1>
+            <p className='description'>{blogPost.description}</p>
+            <div dangerouslySetInnerHTML={{ __html: htmlOutput}} />
             <img src='/raccoon-dance.gif' alt="404"></img>
-            <Link href={'/createBlogPost/' + blogPost[0].id_blogpost}>
+            <Link href={'/createBlogPost/' + blogPost.id_blogpost}>
             <p>Edit BlogPost</p>
             </Link>
         </div>
