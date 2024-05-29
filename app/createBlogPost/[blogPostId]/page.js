aeb3dfd5c05a31347bddbd9f52e9bc720e89bc33
Marek Vykysalý <marek.vykysaly12@gmail.com>
2024-05-29 16:46:07 +0200

    feat: editBlogPost protected route

--- app/createBlogPost/[blogPostId]/page.js
+++ app/createBlogPost/[blogPostId]/page.js
@@ -10,59 +10,85 @@ import Cookies from 'js-cookie';
 import { useRouter } from 'next/navigation';
 
 export default function BlogPost({params}){
+  const router = useRouter();
 
-  const [blogPost, setBlogPost] = useState({})
+  const [blogPost, setBlogPost] = useState({});
 
-    useEffect(() => {
-      (async () => {
-        const accessToken = await getAccessToken(Cookies);
-        const blogRes = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/blogPost/` + params.blogPostId,
-       {next: {revalidate: 5},
-      headers: {
-        "Authorization": `Bearer ${accessToken}`
-      }});
-        const blogPost = await blogRes.json();
-        setBlogPost(blogPost[0]); 
-      })();
+  let blogPostAuthorId = undefined;
+  let meId = undefined;
+
+  const getBlogPost = async() => {
+    const blogRes = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/blogPost/` + params.blogPostId);
+
+    const blogPost = await blogRes.json();
+    blogPostAuthorId = blogPost[0].id_author;
+    setBlogPost(blogPost[0]);
+  }
+
+  const getMe = async () => {
+    const accessToken = await getAccessToken(Cookies);
+
+    const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/user/@me`, {
+        headers: {
+          "Authorization": `Bearer ${accessToken}`
+        }
     });
 
-    const UpdateBlogPost = async(event) =>{
-      event.preventDefault();
+    if (response.status != 200) return;
 
-      const title =  event.target.title.value;
-      const description = event.target.description.value;
-      const content = event.target.content.value;
+    const data = await response.json();
+    meId = data.id_user;
 
-      const accessToken = await getAccessToken(Cookies);
+    console.log(blogPost.id_author);
+  }
 
-      const reqBody = {title: title, description: description,content: content, id_author: 1};
+  useEffect(() => {
+    (async () => {
+      await getBlogPost();
+      await getMe();
 
-      const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/blogPost/` + blogPost.id_blogpost, {
-         method: 'PATCH',
-         headers: {
-             'Content-Type': 'application/json',
-             'Authorization': `Bearer ${accessToken}`
-         },
-        credentials: 'include',
-        body: JSON.stringify(reqBody)
-    });
+      if (meId != blogPostAuthorId || !meId)
+        router.push('/404');
+    })();
+  }, []);
 
-    console.log(reqBody);
-    }
+  const UpdateBlogPost = async(event) =>{
+    event.preventDefault();
 
-    const UpdateMarkdown = (event) => {
-      const htmlPreview = document.getElementById('html-preview');
-      const input = event.target.value;
+    const title =  event.target.title.value;
+    const description = event.target.description.value;
+    const content = event.target.content.value;
+
+    const accessToken = await getAccessToken(Cookies);
+
+    const reqBody = {title: title, description: description,content: content, id_author: 1};
+
+    const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/blogPost/` + blogPost.id_blogpost, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`
+        },
+      credentials: 'include',
+      body: JSON.stringify(reqBody)
+  });
+
+  console.log(reqBody);
+  }
+
+  const UpdateMarkdown = (event) => {
+    const htmlPreview = document.getElementById('html-preview');
+    const input = event.target.value;
+
+    const htmlOutput = marked.parse(input);
+    htmlPreview.innerHTML = DOMPurify.sanitize(htmlOutput,
+      {USE_PROFILES: {html: true}});
+  }
   
-      const htmlOutput = marked.parse(input);
-      htmlPreview.innerHTML = DOMPurify.sanitize(htmlOutput,
-        {USE_PROFILES: {html: true}});
-    }
-    
   
   return (
-    <form className="form" onSubmit={UpdateBlogPost}>
-            <label htmlFor="title">Title</label>
+    <form className="form" onSubmit={UpdateBlogPost} >
+            <label htmlFor="title">Title {blogPost.id_author}</label>
             <input type="text" name="title" placeholder="Title" defaultValue={blogPost.title}/>
             <label htmlFor="dscription">Description</label>
             <textarea type="text" name="description"  rows="5" cols="1" placeholder="Description" defaultValue={blogPost.description}/>
