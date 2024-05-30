ce29f108d214f2509b15756b50beed30f81d01d4
Kryštof <jezdikk@gmail.com>
2024-05-30 22:51:26 +0200

    feat: BlogPost Searching

--- app/blogPostFeed/page.js
+++ app/blogPostFeed/page.js
@@ -1,12 +1,22 @@
 'use client'
 
 import React from 'react'
-
 import { useState, useEffect } from "react";
 import BlogPostCard from '@/components/blogpostCard';
+import { useRouter } from 'next/navigation';
 
 const BlogPostFeed = () => {
   const [blogPosts, setBlogPosts] = useState([]);
+
+  const router = useRouter()
+  
+  const Search = (e) => {
+    e.preventDefault()
+    
+    if(e.target.search.value != ""){
+      router.push("blogPostSearch/" + e.target.search.value)
+    }
+  }
 
   useEffect(() => {
     (async () => {
@@ -21,6 +31,11 @@ const BlogPostFeed = () => {
   return (
   <main className="flex flex-row justify-center w-full min-h-screen">
     <section className="flex flex-col align-center w-5/12 gap-6 mt-10 mb-10">
+      <form onSubmit={Search}>
+        <label htmlFor="search"></label>
+        <input type='text' name='search' placeholder='Search'></input>
+        <button type="submit"></button>
+      </form>
     {blogPosts.map((blogpost, index) =>
       <BlogPostCard key={index} blogpostId={blogpost.id_blogpost} title={blogpost.title} description={blogpost.description} authorId={blogpost.id_author} initDate={blogpost.date} tags={["none", "none", "none"]} />
     )}
